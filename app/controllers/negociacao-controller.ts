133227f3891cc0d745367928e460b64765bc6839
iandruch <iandruch.agro@gmail.com>
2021-12-18 00:01:16 -0300

    versao inicial do curso - aula 1

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,54 @@
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
+import { NegociacoesView } from '../views/negociacoes-view.js';
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
+
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+
+    public adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6){
+            this.negociacoes.adiciona(negociacao);
+            this.limparFormulario();
+            this.atualizaView();
+        } else {
+            this.mensagemView.update('Apenas negociaçoes em dis úteis são aceitas')
+        }
+    }
+
+    private criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        //const date = new Date(this.inputData.value.replace(exp, ','));
+        const date = new Date(this.inputData.value);
+
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    private limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+
+    private atualizaView(): void{
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação registrada com sucesso!');
+    }
+}
