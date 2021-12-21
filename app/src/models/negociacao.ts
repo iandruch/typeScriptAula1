eb50035a29abba322ac93ee3dd07988b90915bb4
iandruch <iandruch.agro@gmail.com>
2021-12-21 19:32:25 -0300

    interface avancado - classe abstrata

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,17 +1,12 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel";
+
+export class Negociacao extends Imprimivel {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
-    ) {}
-
-    get volume(): number {
-        return this.quantidade * this.valor;
-    }
-
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
+    ) {
+        super();
     }
 
     public static criaDe (dateString: string, quantidadeString: string, valorString: string): Negociacao {
@@ -23,4 +18,22 @@ export class Negociacao {
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
     }
+
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+
+    public paraTexto(): string {
+        return `
+        Data: ${this.data},
+        Quantidade: ${this.quantidade},
+        Valor: ${this.valor}
+        `
+    }
+
 }
